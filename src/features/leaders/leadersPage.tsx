d736e88a1a29d43b02d8df2d0ab739ddfb3f225a
Yevhen <livotron@gmail.com>
2025-05-29 04:45:26 +0300

    implemened recursive leaders list

--- src/features/leaders/leadersPage.tsx
+++ src/features/leaders/leadersPage.tsx
@@ -3,48 +3,102 @@ import React from "react";
 import { darkTheme } from "../../App";
 
 const dummyAssociates = [
-  "Іван Петренко",
-  "Олена Ковальчук",
-  "Сергій Іванов",
-  "Марія Шевченко",
-  "Іван Петренко",
-  "Олена Ковальчук",
-  "Сергій Іванов",
-  "Марія Шевченко",
-  "Іван Петренко",
-  "Олена Ковальчук",
-  "Сергій Іванов",
-  "Марія Шевченко",
-  "Іван Петренко",
-  "Олена Ковальчук",
-  "Сергій Іванов",
-  "Марія Шевченко",
-  "Іван Петренко",
-  "Олена Ковальчук",
-  "Сергій Іванов",
-  "Марія Шевченко",
-  "Іван Петренко",
-  "Олена Ковальчук",
-  "Сергій Іванов",
-  "Марія Шевченко",
+  { name: "ВАСИЛЕНКО ВАСИЛЬ", score: 88 },
+  {
+    name: "СВІТЛАНЕНКО СВІТЛАНА",
+    score: 66,
+    supporters: [
+      { name: "ПЕТРЕНКО ПЕТРО", score: 23 },
+      { name: "ІВАН ІВАНОВИЧ ІВАНОВ", score: 11 },
+      { name: "ВАСИЛЕНКО ВАСИЛЬ", score: 4 },
+      { name: "СВІТЛАНЕНКО СВІТЛАНА", score: 1 },
+      {
+        name: "ПЕТРЕНКО ПЕТРО",
+        score: 1,
+        supporters: [
+          { name: "ПЕТРЕНКО ПЕТРО", score: 23 },
+          { name: "ІВАН ІВАНОВИЧ ІВАНОВ", score: 11 },
+          { name: "ВАСИЛЕНКО ВАСИЛЬ", score: 4 },
+          { name: "СВІТЛАНЕНКО СВІТЛАНА", score: 1 },
+          { name: "ПЕТРЕНКО ПЕТРО", score: 1 },
+        ],
+      },
+      { name: "ІВАН ІВАНОВИЧ ІВАНОВ", score: 1 },
+      {
+        name: "ВАСИЛЕНКО ВАСИЛЬ",
+        score: 1,
+        supporters: [
+          { name: "ПЕТРЕНКО ПЕТРО", score: 23 },
+          { name: "ІВАН ІВАНОВИЧ ІВАНОВ", score: 11 },
+          {
+            name: "ВАСИЛЕНКО ВАСИЛЬ",
+            score: 4,
+            supporters: [
+              { name: "ПЕТРЕНКО ПЕТРО", score: 23 },
+              { name: "ІВАН ІВАНОВИЧ ІВАНОВ", score: 11 },
+              { name: "ВАСИЛЕНКО ВАСИЛЬ", score: 4 },
+              { name: "СВІТЛАНЕНКО СВІТЛАНА", score: 1 },
+              { name: "ПЕТРЕНКО ПЕТРО", score: 1 },
+            ],
+          },
+          { name: "СВІТЛАНЕНКО СВІТЛАНА", score: 1 },
+          { name: "ПЕТРЕНКО ПЕТРО", score: 1 },
+        ],
+      },
+      { name: "СВІТЛАНЕНКО СВІТЛАНА", score: 1 },
+    ],
+  },
+  { name: "ПЕТРЕНКО ПЕТРО", score: 66 },
+  { name: "ІВАН ІВАНОВИЧ ІВАНОВ", score: 66 },
+  { name: "ВАСИЛЕНКО ВАСИЛЬ", score: 66 },
+  { name: "СВІТЛАНЕНКО СВІТЛАНА", score: 66 },
+  { name: "ПЕТРЕНКО ПЕТРО", score: 66 },
+  { name: "ІВАН ІВАНОВИЧ ІВАНОВ", score: 66 },
+  { name: "ВАСИЛЕНКО ВАСИЛЬ", score: 66 },
+  { name: "СВІТЛАНЕНКО СВІТЛАНА", score: 66 },
+  { name: "ПЕТРЕНКО ПЕТРО", score: 66 },
+  { name: "ІВАН ІВАНОВИЧ ІВАНОВ", score: 1 },
+  { name: "ВАСИЛЕНКО ВАСИЛЬ", score: 1 },
+  { name: "СВІТЛАНЕНКО СВІТЛАНА", score: 1 },
+  { name: "ПЕТРЕНКО ПЕТРО", score: 1 },
+  { name: "ІВАН ІВАНОВИЧ ІВАНОВ", score: 1 },
+  { name: "ВАСИЛЕНКО ВАСИЛЬ", score: 1 },
+  { name: "СВІТЛАНЕНКО СВІТЛАНА", score: 1 },
+  { name: "ПЕТРЕНКО ПЕТРО", score: 1 },
+  { name: "ІВАН ІВАНОВИЧ ІВАНОВ", score: 1 },
+  { name: "ВАСИЛЕНКО ВАСИЛЬ", score: 1 },
+  { name: "СВІТЛАНЕНКО СВІТЛАНА", score: 1 },
+  { name: "ПЕТРЕНКО ПЕТРО", score: 1 },
+  { name: "ІВАН ІВАНОВИЧ ІВАНОВ", score: 1 },
 ];
 
-const LeadersPage = () => (
-  <List style={{ padding: 0 }}>
-    {dummyAssociates.map((name, idx) => (
+type User = {
+  name: string;
+  score: number;
+  supporters?: User[];
+};
+
+const supportersList = (user: User, nestedLevel: number) => {
+  const newNestedLevel = nestedLevel + 1;
+  return (
+    user.supporters &&
+    user.supporters.map((supporter) => (
       <ListItem
-        key={name}
+        key={user.name}
         style={{
-          paddingTop: 4,
-          paddingBottom: 4,
-          paddingLeft: 0,
-          paddingRight: 0,
+          padding: 0,
+          marginLeft: 16,
+          flexDirection: supporter.supporters ? "column" : "row",
+          alignItems: supporter.supporters ? "flex-start" : "center",
         }}
       >
         <ListItemText
           primary={
             <span
-              style={{ display: "flex", alignItems: "center", fontWeight: 700 }}
+              style={{
+                display: "flex",
+                alignItems: "center",
+              }}
             >
               <span
                 className="bullet-number"
@@ -54,24 +108,79 @@ const LeadersPage = () => (
                   justifyContent: "center",
                   width: 32,
                   height: 32,
-                  borderRadius: "50%",
+                  borderRadius: supporter.supporters ? "0" : "50%",
                   background: darkTheme.palette.primary.main,
                   color: darkTheme.palette.secondary.main,
                   fontSize: 18,
-                  marginRight: 16,
-                  border: `2px solid ${darkTheme.palette.secondary.main}`,
+                  marginRight: 8,
+                  marginTop: 4,
+                  marginBottom: 4,
                   boxSizing: "border-box",
                 }}
               >
-                {idx + 99}
+                {supporter.score}
               </span>
-              {name}
+              <Typography>{supporter.name}</Typography>
             </span>
           }
         />
+        <List style={{ padding: 0 }}>
+          {supportersList(supporter, newNestedLevel)}
+        </List>
       </ListItem>
-    ))}
-  </List>
-);
+    ))
+  );
+};
+
+const LeadersPage = () => {
+  const nestedLevel = 0;
+  return (
+    <List style={{ padding: 0 }}>
+      {dummyAssociates.map((user, idx) => (
+        <ListItem
+          key={user.name}
+          style={{
+            padding: 0,
+            paddingRight: 0,
+            flexDirection: user.supporters ? "column" : "row",
+            alignItems: user.supporters ? "flex-start" : "center",
+          }}
+        >
+          <ListItemText
+            primary={
+              <span style={{ display: "flex", alignItems: "center" }}>
+                <span
+                  className="bullet-number"
+                  style={{
+                    display: "inline-flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    width: 32,
+                    height: 32,
+                    borderRadius: user.supporters ? "0" : "50%",
+                    background: darkTheme.palette.primary.main,
+                    color: darkTheme.palette.secondary.main,
+                    fontSize: 18,
+                    marginRight: 8,
+                    marginTop: 4,
+                    marginBottom: 4,
+                    border: `2px solid ${darkTheme.palette.secondary.main}`,
+                    boxSizing: "border-box",
+                  }}
+                >
+                  {user.score}
+                </span>
+                <Typography>{user.name}</Typography>
+              </span>
+            }
+          />
+          <List style={{ padding: 0 }}>
+            {supportersList(user, nestedLevel)}
+          </List>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
 
 export default LeadersPage;
